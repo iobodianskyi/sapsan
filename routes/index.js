db3acaf5e5593e810d813b3c4ff3041e01d7c43c
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2018-04-15 00:09:35 +0300

    Clean up and refactor project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+(function () {
+  var express = require('express');
+  var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  /* GET home page. */
+  router.get('/', function (req, res) {
+    res.render('index', {title: 'СК Сапсан - рукопашний бій у Чечельницькому ройоні'});
+  });
 
-module.exports = router;
+  module.exports = router;
+
+})();
