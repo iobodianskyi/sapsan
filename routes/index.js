527ac89ec71186606d18c005a52a08444d83b581
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2019-03-21 14:28:05 +0200

    Added about page

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@
 
   /* GET home page. */
   router.get('/', function (req, res) {
-    res.render('index', {title: 'СК Сапсан - рукопашний бій у Чечельницькому районі'});
+    res.render('index', { title: 'СК Сапсан - рукопашний бій у Чечельницькому районі' });
   });
 
-  /* GET coach page. */
+  // GET coach page
   router.get('/coach', function (req, res) {
-    res.render('coach', {title: 'Таранець Дмитро - тренер СК Сапсан'});
+    res.render('coach', { title: 'Таранець Дмитро - тренер СК Сапсан' });
   });
+
+  // GET about page.
+  router.get('/about', function (req, res) {
+    res.render('about', { title: 'Про клуб Сапсан' });
+  });
+
 
   module.exports = router;
 
